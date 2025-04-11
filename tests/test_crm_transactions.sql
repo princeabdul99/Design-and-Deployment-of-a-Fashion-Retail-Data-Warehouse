6871d6e5cd174502ea09b8c21cc0188e242ef84e
Abubakar Abdullahi <ajanifiles90@gmail.com>
2025-04-11 21:21:10 +0800

    Update test_crm_transactions.sql

--- tests/test_crm_transactions.sql
+++ tests/test_crm_transactions.sql
@@ -1,4 +1,21 @@
-﻿USE DatawarehouseFashionRetail;
+﻿/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/    
 
 
   --- Checking for NULLs
@@ -54,64 +71,6 @@ WHERE date < '1924-01-01'
 -- Check total number of row records
 -- Expectation: match the total row record on bronze table
 SELECT COUNT(*) FROM silver.crm_transactions ;
-
-
-
-SELECT TOP (100)
-	 t.invoiceid,
-t.line,
-t.customerid,
-t.productid,
-CASE WHEN size IS NULL THEN 'n/a'
-	ELSE size
-END AS size, 
-CASE WHEN color IS NULL THEN 'n/a' 
-	ELSE color
-END AS color,
-t.unitprice,
-t.quantity,
-t.date,
-t.discount,
-CASE WHEN linetotal IS NULL OR linetotal <= 0 OR linetotal != ABS(unitprice) * quantity * (1-discount) -- LineTotal = Unit Price x Quantity x (1-Discount)
-	THEN ABS(unitprice) * quantity * (1-discount)
-	ELSE  linetotal
-END AS linetotal,
-t.storeid,
-t.employeeid,
-t.currency,
-CASE WHEN currency = 'EUR' THEN '€'
-	 WHEN currency = 'GBP' THEN '₤'
-	 WHEN currency = 'CNY' THEN '¥'
-	 WHEN currency = 'USD' THEN '$'
-	ELSE 'n/a'
-END AS currencysymbol,      
-t.sku,
-
-CASE 
-	WHEN RIGHT(sku, 2) = '--' THEN LEFT(sku, LEN(sku) - 2)
-	WHEN RIGHT(sku, 1) = '-' THEN LEFT(sku, LEN(sku) - 1)
-    ELSE sku
-END AS cleaned_sku,
-
-t.transactiontype,
-t.paymentmethod,
-    agg.InvoiceTotal
-FROM 
-    bronze.crm_transactions t
-LEFT JOIN (
-    SELECT 
-        invoiceid,
-        SUM(
-            CASE 
-                WHEN transactiontype = 'Sale' THEN ABS(unitprice) * quantity * (1 - discount)
-                WHEN transactiontype = 'Return' THEN -1 * ABS(unitprice) * quantity * (1 - discount)
-                ELSE 0
-            END
-        ) AS InvoiceTotal
-    FROM bronze.crm_transactions
-    GROUP BY invoiceid
-) agg
-ON t.invoiceid = agg.invoiceid
 
 
 SELECT TOP (100) * FROM silver.crm_transactions where productid = 1795;
