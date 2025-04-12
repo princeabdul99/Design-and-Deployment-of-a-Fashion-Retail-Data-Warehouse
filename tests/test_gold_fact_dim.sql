ea1ee5f7c33b9b510a5d75aee4b6e382d58f8f1c
Abubakar Abdullahi <ajanifiles90@gmail.com>
2025-04-12 22:21:34 +0800

    Update test_gold_fact_dim.sql

--- tests/test_gold_fact_dim.sql
+++ tests/test_gold_fact_dim.sql
@@ -1,5 +1,23 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
 
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
 
+-- ====================================================================
+-- Checking 'gold.fact_sales'
+-- ====================================================================
+-- Check the data model connectivity between fact and dimensions
 -- Foreign Key Integrity (Dimension)
  --- Expectation: No Result
 
@@ -26,3 +44,69 @@ FROM gold.fact_sales f
 LEFT JOIN gold.dim_products c
 ON c.product_id = f.product_id
 WHERE c.product_id IS NULL;
+
+
+
+-- ====================================================================
+-- Checking 'gold.dim_customers'
+-- ====================================================================
+-- Check for Uniqueness of Customer Id in gold.dim_customers
+-- Expectation: No results 
+SELECT 
+    customer_id,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_id
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.dim_products'
+-- ====================================================================
+-- Check for Uniqueness of Product Id in gold.dim_products
+-- Expectation: No results 
+SELECT 
+    product_id,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_id
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.dim_stores'
+-- ====================================================================
+-- Check for Uniqueness of Store Id in gold.dim_stores
+-- Expectation: No results 
+SELECT 
+    store_id,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_stores
+GROUP BY store_id
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.dim_employees'
+-- ====================================================================
+-- Check for Uniqueness of Employee Id in gold.dim_employees
+-- Expectation: No results 
+SELECT 
+    employee_id,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_employees
+GROUP BY employee_id
+HAVING COUNT(*) > 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
